0c573d4a460f8f1b3d125de8a24ce3d69516cb03
parinitaShirke <131198150+parinitaShirke@users.noreply.github.com>
2023-08-16 11:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 
 	let fname = document.querySelector("#form1 input[name="fname"]");
-	let lname = document.querySelector("#form2 input[name="lname"]");
+	let lname = document.querySelector("#form1 input[name="lname"]");
 	alert(fname+" "+lname);
 }
